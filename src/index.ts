ea87f57cd40a31f90573034ca2911d5c3b282fb0
Omori taita <Omori@MacBook.local>
2021-12-04 18:39:50 +0900

    Set up typescript

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { randomInt } from "crypto"
+
+console.log("Hello Typescript!")
+
+let a = 1 + 2
+let b = a + 3
+let c = {
+    apple: a,
+    banana: b
+}
+let d = c.apple * 4
+
+let e = randomInt(1, 6)
