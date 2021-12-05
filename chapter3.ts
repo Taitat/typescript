657980abc0b85889702192e3f54fd5c8d5aaab6c
Taita Omori <taita.omori@gmail.com>
2021-12-05 17:58:58 +0900

    Modify argument types for call function.

--- chapter3.ts
+++ chapter3.ts
@@ -19,3 +19,16 @@ let reserve: Reserve = (
         // 即出発
     }
 }
+
+function call <T extends [unknown, string, ...unknown[]], U, R> (
+    f: (...args: T) => R,
+    ...args: T
+): R {
+    return f(...args)
+}
+
+function fill(length: number, value: string): string[] {
+    return Array.from({length}, () => value)
+}
+
+call(fill, 10, 'a')
