62dbc0f1b79f14a266e6ddb9902f10a6dda9a75d
Taita Omori <taita.omori@gmail.com>
2021-12-05 18:47:31 +0900

    Make chess engine.

---
+++ chapter5.ts
@@ -0,0 +1,50 @@
+type Color = 'Black' | 'White'
+type Files = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H'
+type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+
+
+class Game {
+    private pieces = Game.makePieces()
+    private static makePieces() {
+        return [
+            new King('White', 'E', 1),
+            new King('Black', 'E', 8)
+        ]
+    }
+}
+
+abstract class Piece {
+    protected position: Position
+    constructor(
+        private readonly color: Color,
+        file: Files,
+        rank: Rank
+    ) {
+        this.position = new Position(file, rank)
+    }
+    moveTo(position: Position) {
+        this.position = position
+    }
+    abstract canMoveTo(position: Position): boolean
+}
+
+class Position {
+    constructor(
+        private file: Files,
+        private rank: Rank
+    ) {}
+    distanceFrom(postion: Position) {
+        return {
+            rank: Math.abs(postion.rank - this.rank),
+            file: Math.abs(postion.file.charCodeAt(0) - this.file.charCodeAt(0))
+        }
+    }
+}
+
+
+class King extends Piece {
+    canMoveTo(position: Position){
+        let distance = this.position.distanceFrom(position)
+        return distance.rank < 2 && distance.file < 2
+    }
+}
